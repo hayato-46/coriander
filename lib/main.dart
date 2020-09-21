510bdde2687a02b57080ba0eabf750cc697da5a5
hayato-46 <hayato468810macpro@gmail.com>
2020-09-21 15:29:59 +0900

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,46 @@
+import 'package:coriander/main_model.dart';
 import 'package:coriander/next_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
+
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('コリアンダー'),
+          ),
+          body: Consumer<MainModel>(builder: (context, model,child) {
+              return Center(
+                child: Column(
+                  children:<Widget> [
+                    Text(
+                        model.kboyText,
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text('ボタン'),
+                      onPressed: (){
+                        //todo//
+                        model.changeKboyText();
+                      },
+                    ),
+                  ],
+                ),
+              );
+            }
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  final items = [];
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('内田真礼勉強中'),
-        backgroundColor: Colors.black,
-        actions: <Widget>[
-        ],
-      ),
-      body: Container(
-        width: double.infinity,
-        child: GridView.count(
-          // Create a grid with 2 columns. If you change the scrollDirection to
-          // horizontal, this produces 2 rows.
-          crossAxisCount: 3,
-          // Generate 100 widgets that display their index in the List.
-          children: List.generate(100, (index) {
-            return Column(
-              children: <Widget>[
-                Text('東岡さんの嫁$index'),
-                Expanded(
-                    child: Image.network('https://pbs.twimg.com/media/EhZscxtU8AQIgXA?format=jpg&name=medium'))
-              ],
-            );
-          }),
-        ),
-        ),
-      );
   }
 }
